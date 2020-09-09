b7a797fa2c72647fd0e95bba68411281cd52aebc
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2020-09-09 14:51:37 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,18 +6,25 @@ window.onload = function fadeOutEffect() {
 		fadeTarget.style.display = "none"
 	}, 500);
 }
-
+ 
 var myNav = document.getElementById('navbar');
+var navlink = document.getElementById("nav-link");
 window.onscroll = function () { 
     "use strict";
     if (document.documentElement.scrollTop >= 480 ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
+        navlink.style.borderTop = "1px solid black";
     }
 	if(document.documentElement.scrollTop < 480 ){
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
+        navlink.style.borderTop = "1px solid #ed8936";
 	}
 
 };
+window.scroll({
+     
+  behavior: 'smooth'
+});
 
